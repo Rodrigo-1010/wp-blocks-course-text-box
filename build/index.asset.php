b2072c3d9bac23054767420ce2ac2de7136b8439
rodrigo <rodrigo@cakedidit.com>
2023-02-28 11:32:52 -0300

    initial commit

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1e38b8ea60f21044673e98dc656f111a');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '869be83c28fef624860f22fb4038dab0');
